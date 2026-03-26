d55d93afbcb6390b357cc2af70ff714bed55baac
Meenakshi Adiandra <meenakshiadiandra199905@gmail.com>
2026-03-26 17:53:15 +0530

    Fix: Headless Chrome for GitHub Actions

--- conftest.py
+++ conftest.py
@@ -14,6 +14,7 @@ def driver():
     options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1920,1080")
 
     driver = webdriver.Chrome(
         service=Service(ChromeDriverManager().install()),
